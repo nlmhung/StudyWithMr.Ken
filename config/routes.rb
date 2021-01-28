982ee4e60557cec7d23b0935738b46fa91d72038
nlmhung <71160837+nlmhung@users.noreply.github.com>
2021-01-28 15:37:45 +0700

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :managers
+  # devise_for :managers
   root 'movies#index'
   
   resources :movies
   resources :genres
-  resources :managers
+  # resources :managers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
